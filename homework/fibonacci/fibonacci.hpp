7b74133e93b40cf556801f0e56a5f069b8891935
karol <karol3129@gmail.com>
2025-06-03 20:46:52 +0200

    Added implementation of the fibonacci functions

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,38 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result{0};
+    int resultLast{0};
+    int resultLastLast{0};
+
+    if (sequence == 0) {
+        return 0;
+    }
+
+    if (sequence == 1) {
+        return 1;
+    }
+
+    resultLast = 1;
+
+    for (int i = 2; i <= sequence; ++i) {
+        result = resultLast + resultLastLast;
+
+        resultLastLast = resultLast;
+        resultLast = result;
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    }
+
+    if (sequence == 1) {
+        return 1;
+    }
+
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
