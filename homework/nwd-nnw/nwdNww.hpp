b4bab3339350d4c348e34784cce0367072b7b5c0
karol <karol3129@gmail.com>
2025-06-03 21:04:34 +0200

    Added implementation of the NWD and NWW functions

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if ((lhs == 0) && (rhs == 0))
+    {
+        return 0;
+    }
+    else if ((lhs == 0) && (rhs != 0)) {
+        return std::abs(rhs);
+    }
+    else if ((lhs != 0) && (rhs == 0))
+    {
+        return std::abs(lhs);
+    }
+
+    int lastDivider{1};
+    int malhsDivider{std::min(std::abs(lhs), std::abs(rhs))};
+
+    for (int i = 1; i <= malhsDivider; ++i) {
+        if (((lhs % i) == 0) && ((rhs % i) == 0)) {
+            lastDivider = i;
+        }
+    }
+
+    return lastDivider;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if((lhs == 0) && (rhs == 0))
+    {
+        return 0;
+    }
+    return std::abs(lhs * rhs) / NWD(lhs, rhs);
 }
