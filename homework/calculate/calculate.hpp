13393f95c964d64385e61386a31ea1c9f955fcbb
karol <karol3129@gmail.com>
2025-06-03 20:42:26 +0200

    Fixed formating.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,30 +1,19 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second)
-{
-    if(command == "add")
-    {
-        return std::to_string( first + second );
-    }
-    else if (command == "subtract")
-    {
-        return std::to_string( first - second );
-    }
-    else if (command == "multiply")
-    {
-        return std::to_string( first * second );
-    }
-    else if (command == "divide")
-    {   
-        if(second == 0)
-        {
+std::string calculate(const std::string& command, int first, int second) {
+    if (command == "add") {
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else if (command == "multiply") {
+        return std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second == 0) {
             return std::string{"Division by 0"};
         }
-        return std::to_string( first / second );
-    }
-    else
-    {
+        return std::to_string(first / second);
+    } else {
         return std::string{"Invalid data"};
     }
 }
