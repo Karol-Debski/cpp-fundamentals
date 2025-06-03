48dd3da1136e8e62845eee9c05d88eab1e30dc0d
karol <karol3129@gmail.com>
2025-06-03 22:27:52 +0200

    Fixed formating.

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,9 +4,8 @@
 int addEven(const std::vector<int>& numbers) {
     int sum{0};
 
-    for(const auto& e : numbers)
-    {
-        sum += ( (e % 2) == 0 ) ? e : 0;     
+    for (const auto& e : numbers) {
+        sum += ((e % 2) == 0) ? e : 0;
     }
 
     return sum;
